c74db1a9b6f688a3b46dc0546292138a072a4433
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-12-19 23:10:56 -0500

    complete crub lab but delete functionality not working in browser

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,9 @@
+song1 = Song.create(title: "Hymn", released: true, release_year: 2017, artist_name: "Kesha", genre: "pop")
+
+song2 = Song.create(title: "Timber", released: true, release_year: 2015, artist_name: "Kesha", genre: "pop")
+
+song3 = Song.create(title: "House Party", released: true, release_year: 2014, artist_name: "Sam Hunt", genre: "country")
+
+song4 = Song.create(title: "Cop Car", released: true, release_year: 2015, artist_name: "Sam Hunt", genre: "country")
+
+song5 = Song.create(title: "I'm the One", released: true, release_year: 2017, artist_name: "DJ Kahlid", genre: "rap")
